92f69b2dbf6e646f9298419c2be6385f81cb6386
Jacob David C Cunningham <jcun4128@gmail.com>
2020-07-16 23:05:35 -0500

    fix for cron path
    env is undefined

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
-require('dotenv').config();
+const dotenv = require('dotenv');
+dotenv.config({ path: __dirname + '/.env' });
 const fs = require('fs');
 const axios = require('axios');
 const dateTime = require('node-datetime');
